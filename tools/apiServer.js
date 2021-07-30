d96dc602b500e9e72e57d2fc20e9c3f934f35659
GautamKumar <soewin1990@gmail.com>
2021-07-31 00:20:59 +0800

    update readme

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -58,7 +58,7 @@ server.post("/courses/", function (req, res, next) {
 server.use(router);
 
 // Start server
-const port = 4001;
+const port = 3001;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
